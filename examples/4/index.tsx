32d28a58db005dbbad53026f98c76d4bc28c9b25
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-02-27 21:37:38 -0500

    add ability to handle the creation of Fragment JSX elements (array of elements) in HyperScript

--- examples/4/index.tsx
+++ examples/4/index.tsx
@@ -12,9 +12,10 @@ const
 
 /** in the esbuild build options (`BuildOptions`), you must set `jsxFactory = "h"` and `jsxFragment = "Fragment"` */
 export const h = ctx.addClass(HyperScript_Signal)
+export const Fragment = (props: object, ...elements: Node[]) => (elements)
 
 // TODO: implement `onDelete` cleanup
-export const MyComponent = () => {
+export const MyComponents = () => {
 	const [, getCount, setCount] = createState(0)
 	const [, getStyle, setStyle] = createState("background-color: red;")
 	setInterval(() => {
@@ -26,6 +27,16 @@ export const MyComponent = () => {
 	setTimeout(() => {
 		setStyle("background-color: blue;")
 	}, 6000)
-	return <div style={getStyle} >current time is: {getCount}</div>
+	return <>
+		<div style={getStyle} >current time is: {getCount}</div>
+		<div style={getStyle} >current time is: {getCount}</div>
+	</>
 }
 
+export const MyComponent = () => {
+	const component_fragment = MyComponents()
+	setTimeout(() => {
+		document.body.appendChild(component_fragment[0])
+	}, 4000)
+	return component_fragment[1]
+}
--- examples/jsx_transform4.ts
+++ examples/jsx_transform4.ts
@@ -1,4 +1,4 @@
-import { StaticImplements, Stringifyable, isFunction, object_entries } from "../src/deps.ts"
+import { StaticImplements, Stringifyable, array_isArray, isFunction, object_entries } from "../src/deps.ts"
 import { DOMAttrSignal_Factory, DOMTextNodeSignal_Factory } from "../src/dom_signal.ts"
 import { Context } from "../src/mod.ts"
 import { Accessor } from "../src/typedefs.ts"
@@ -7,13 +7,18 @@ import { Accessor } from "../src/typedefs.ts"
 export type ReactiveText = Stringifyable | Accessor<Stringifyable>
 /** the `props` of an `HTMLElement` are simply their attributes */
 export type ElementAttrs = { [attribute_key: Attr["name"]]: ReactiveText }
-export type ComponentGenerator<P = {}> = (props?: P) => Element
+export type SingleComponentGenerator<P = {}> = (props?: P) => Element
+export type FragmentComponentGenerator<P = {}> = (props?: P) => Element[]
+export type ComponentGenerator<P = {}> = SingleComponentGenerator<P> | FragmentComponentGenerator<P>
 export type ElementChild = ReactiveText | Node
+export type ElementChildren = Array<ElementChild | ElementChild[]>
 
 interface HyperScriptInterface {
 	new(): object
-	create(html_tag: string, attrs?: ElementAttrs | null, ...children: ElementChild[]): HTMLElement
-	create<P = {}>(component: ComponentGenerator<P>, props?: P | null, ...children: ElementChild[]): Node
+	create(html_tag: string, attrs?: ElementAttrs | null, ...children: ElementChildren): HTMLElement
+	create<P = {}>(component: SingleComponentGenerator<P>, props?: P | null, ...children: ElementChildren): Element
+	create<P = {}>(fragment_component: FragmentComponentGenerator<P>, props?: P | null, ...siblings: ElementChildren): Element[]
+	create<P = {}>(component: ComponentGenerator<P>, props?: P | null, ...children: ElementChildren): ReturnType<typeof component>
 }
 
 export const HyperScript_Signal = (ctx: Context) => {
@@ -22,26 +27,33 @@ export const HyperScript_Signal = (ctx: Context) => {
 		createAttr = ctx.addClass(DOMAttrSignal_Factory)
 	return class HyperScript implements StaticImplements<HyperScriptInterface, typeof HyperScript> {
 		constructor() { }
-		static create(html_tag: string, attrs?: ElementAttrs | null, ...children: ElementChild[]): HTMLElement
-		static create<P = {}>(component: ComponentGenerator<P>, props?: P | null, ...children: ElementChild[]): Node
-		static create(component: string | ComponentGenerator, props?: ElementAttrs | null, ...children: ElementChild[]): HTMLElement {
+		static create(html_tag: string, attrs?: ElementAttrs | null, ...children: ElementChildren): HTMLElement
+		static create<P = {}>(component: SingleComponentGenerator<P>, props?: P | null, ...children: ElementChildren): Element
+		static create<P = {}>(fragment_component: FragmentComponentGenerator<P>, props?: P | null, ...siblings: ElementChildren): Element[]
+		static create(component: string | ComponentGenerator, props?: ElementAttrs | null, ...children: ElementChild[]): Element | Element[] {
 			props ??= {}
 			const
-				is_component_generator = isFunction(component),
-				component_node: Element = is_component_generator ? component(props) : document.createElement(component)
-			// we only assign the props as attributes iff the created node did not come from a component generator. this is also the behavior exhibited by ReactJS.
-			if (!is_component_generator) {
-				// assign the props as reactive attributes of the new element node
-				for (const [attr_name, attr_value] of object_entries(props)) {
-					component_node.setAttributeNode(createAttr(attr_name, attr_value)[2])
-				}
-			}
-			children.forEach((child) => {
-				component_node.appendChild(child instanceof Node
+				child_nodes = children.flatMap((child) => child instanceof Node
 					? child
 					: createText(child)[2]
-				)
-			})
+				),
+				is_component_generator = isFunction(component),
+				component_node: Element | Element[] = is_component_generator ? component(props) : document.createElement(component)
+			// now, depending on whether or not the `component_node` is a fragment (i.e. an array of nodes),
+			// we append the children as child elements if the `component_node` is a single `Element`,
+			// or we insert the children as siblings if the `component_node` is an array of elements (`Element[]`)
+			if (!array_isArray(component_node)) {
+				// we only assign the props as attributes iff the created node did not come from a component generator. this is also the behavior exhibited by ReactJS.
+				if (!is_component_generator) {
+					// assign the props as reactive attributes of the new element node
+					for (const [attr_name, attr_value] of object_entries(props)) {
+						component_node.setAttributeNode(createAttr(attr_name, attr_value)[2])
+					}
+				}
+				component_node.append(...child_nodes)
+			} else {
+				component_node.push(...child_nodes as Element[])
+			}
 			return component_node as HTMLElement
 		}
 	}
