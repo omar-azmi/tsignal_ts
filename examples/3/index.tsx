479be37175008b824aa4f782273a87f95ba7d8fc
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-02-28 16:39:54 -0500

    add `/examples/3/` of an svg clock. this time around I did not mimic solidjs's way of doing things, because handling props with my library is an extremly manual process with my library. so I kept it simple and not very configurable/customizable.

---
+++ examples/3/index.tsx
@@ -0,0 +1,56 @@
+/** @jsx h */
+/** @jsxFrag hf */
+
+import { createHyperScript } from "../../src/jsx/hyperscript.ts"
+import { Context, MemoSignal_Factory, StateSignal_Factory } from "../../src/mod.ts"
+
+const
+	ctx = new Context(),
+	createState = ctx.addClass(StateSignal_Factory),
+	createMemo = ctx.addClass(MemoSignal_Factory)
+
+/** in the esbuild build options (`BuildOptions`), you must set `jsxFactory = "h"` and `jsxFragment = "hf"` */
+export const [h, hf, changeNS] = createHyperScript(ctx)
+
+type TimeState = {
+	hour: number
+	minute: number
+	second: number
+}
+
+const App = () => {
+	const
+		now_time = new Date(),
+		today_midnight_epochtime = new Date(now_time.getFullYear(), now_time.getMonth(), now_time.getDate(), 0, 0, 0).getTime(),
+		[, getEpochTime, setEpochTime] = createState<number>(0),
+		[, seconds_since_midnight] = createMemo((id) => {
+			return ((getEpochTime(id) - today_midnight_epochtime) / 1000) | 0
+		})
+	const [, currentTime] = createMemo<TimeState>((id) => {
+		const s = seconds_since_midnight(id)
+		return {
+			second: s % (60),
+			minute: ((s / 60) % 60) | 0,
+			hour: ((s / (60 * 60)) % 12) | 0,
+		}
+	}, { equals: false })
+
+
+	setInterval(() => setEpochTime(Date.now()), 500)
+
+	// we must change the namespace to `svg`, so that hypescript picks up on it, and handles the newly created svg nodes appropriately.
+	changeNS("svg")
+	const svg_dom = <svg width="200px" height="200px" viewBox="0 0 200 200">
+		<g transform="translate(100, 100)">
+			<circle r="100" fill="lightgrey" stroke="black" />
+			<line transform={createMemo((id) => `rotate(${currentTime(id).second * 6})`)[1]} class="hand-seconds" y1="0" y2="-100" stroke="red" stroke-width={4} />
+			<line transform={createMemo((id) => `rotate(${currentTime(id).minute * 6})`)[1]} class="hand-minutes" y1="0" y2="-100" stroke="green" stroke-width={4} />
+			<line transform={createMemo((id) => `rotate(${currentTime(id).hour * 5 * 6})`)[1]} class="hand-hours" y1="0" y2="-100" stroke="blue" stroke-width={4} />
+		</g>
+	</svg>
+	// declare that the svg namespace is now over, and switch back to html namespace
+	changeNS()
+	return svg_dom
+}
+
+document.getElementById("root")!.append(App())
