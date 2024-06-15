1004ac23b09cd17dfd789d0b6f31db59c0633461
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-06-15 15:10:36 +0300

    restructure how the `UnisetStateSignal` was implemented. the new pattern looks more feasible, as it: - separates the logic between:   - when the signal is to be fired independently due to a mutation in the underlying data structure that is carrying the collection of `Accessor`s. (this is what the `Uniset` class does)   - when the signal needs to be fired as a result of one of its dependencies changing. (this is what the `UnisetSignal` class does) - moreover, it allows us to extend the data-structure which we wish to remix directly.   thus allowing us to override super class methods, and also call `super` methods.   it becomes far more clear this way which methods result in a potential mutation and will need to initiate the firing of an update cycle. - finally, in the `static create` method of the signal, it allows us to simply return the entire proxy-like data structure as a single item, rather than returning an array of bound closure methods.   the advantage of this is that data structures with a ton of mutation methods (like `Array`s, with `push`, `pop`, `shift`, etc...) will not need to have all of their methods first bound, and then returned somewhere in a very long array of bound methods.   it is simply unsustainable. - update the complementary test file to use the new signature of the `static create` signal method.

--- src/collection_signal.ts
+++ src/collection_signal.ts
@@ -7,19 +7,97 @@
 */
 
 import type { Context } from "./context.ts"
+import { object_assign } from "./deps.ts"
 import { MemoSignal_Factory, type SimpleSignalConfig } from "./signal.ts"
-import type { Accessor, Identifiable, PureAccessor } from "./typedefs.ts"
+import type { Accessor, ID, Identifiable, PureAccessor } from "./typedefs.ts"
 
+/** this information is needed by the proxy-like data-structures that run signals when they are mutated. <br>
+ * see {@link Uniset} for an example.
+*/
+interface BoundSignalInfo {
+	id: ID
+	addEdge: Context["addEdge"]
+	delEdge: Context["delEdge"]
+	runId: Context["runId"]
+}
+
+/** a unique set of {@link Accessor | Accessors}, that implements all builtin `Set` class methods, and fires a signal update upon mutation. */
+class Uniset<A extends Accessor<any>> extends Set<A> implements Identifiable<{}> {
+	declare public readonly id: number
+	declare protected readonly addEdge: (src_id: number, dst_id: number) => boolean
+	declare protected readonly delEdge: (src_id: number, dst_id: number) => boolean
+	declare protected readonly runId: (id: number) => boolean
+
+	constructor(config: BoundSignalInfo, items: Iterable<A> = []) {
+		// we do not pass `items` to the super constructor, because it internally uses `this.add` to add the items,
+		// and as we know, since that method is overloaded here, and requires the `config` to be assigned to `this` before being used,
+		// and we can't assign `config` to `this` before calling the super constructor,
+		// so the only way out is by not adding the initial items via the super constructor, and instead adding them manually, one by one later on.
+		super()
+		object_assign(this, config)
+		const { addEdge, id } = config
+		// the initial items are added as a dependency, but they will NOT trigger an update cycle via `runId`.
+		// this is because we want the user of this class to decide whether or not it should fire initially.
+		// the default behavior is similar to `defer`ing
+		for (const item of items) {
+			super.add(item)
+			addEdge(item.id, id)
+		}
+	}
+
+	addItems(...items: A[]): void {
+		const { id, addEdge, runId } = this
+		let mutated = false
+		items.forEach((item) => {
+			if (!super.has(item)) {
+				super.add(item)
+				addEdge(item.id, id)
+				mutated = true
+			}
+		})
+		if (mutated) { runId(id) }
+	}
+
+	delItems(...items: A[]): void {
+		const { id, delEdge, runId } = this
+		let mutated = false
+		items.forEach((item) => {
+			if (super.delete(item)) {
+				delEdge(item.id, id)
+				mutated = true
+			}
+		})
+		if (mutated) { runId(id) }
+	}
+
+	add(value: A): this {
+		this.addItems(value)
+		return this
+	}
+
+	delete(value: A): boolean {
+		const item_exists = super.has(value)
+		this.delItems(value)
+		return item_exists
+	}
+
+	clear(): void {
+		// the following is not an efficient implementation.
+		// it could have been faster if I had exposed `fmap` and `rmap` from the `Context`, but I don't want to do that.
+		this.delItems(...this)
+	}
+}
 
 /** creates a signal that internally maintains a `Set` collection of {@link Accessor | `Accessors\<T\>`}, but returns a `Set<T>` when accessed (via the getter function). <br>
  * this signal will fire an update whenever one of its constituent signal items fires an update. <br>
- * this signal also fires independently when either _new_ {@link Accessor} item(s) are added (via the {@link UnisetStateSignal.addItems}), or when _old_ item(s) are removed (via the {@link UnisetStateSignal.delItems}).
+ * this signal also fires independently when either _new_ {@link Accessor} item(s) are added (via the {@link UnisetSignal.items.addItems}), or when _old_ item(s) are removed (via the {@link UnisetSignal.items.delItems}).
 */
-export const UnisetStateSignal_Factory = (ctx: Context) => {
+export const UnisetSignal_Factory = (ctx: Context) => {
 	const { runId, addEdge, delEdge } = ctx
-	//@ts-ignore: the function signature of the static `create` method is different from its base class. but we don't care.
-	return class UnisetStateSignal<T> extends ctx.getClass(MemoSignal_Factory)<Set<T>> {
-		declare items: Set<Accessor<T>>
+	/** {@inheritDoc UnisetSignal_Factory} */
+	// @ts-ignore: the function signature of the static `create` method is different from its base class. but we don't care.
+	return class UnisetSignal<T> extends ctx.getClass(MemoSignal_Factory)<Set<T>> {
+		declare items: Uniset<Accessor<T>>
 		declare value: Set<T>
 
 		constructor(
@@ -32,35 +110,9 @@ export const UnisetStateSignal_Factory = (ctx: Context) => {
 				items.forEach((item) => { value.add(item(0)) })
 				return value
 			}, { ...config, value: new Set(), defer: true, equals: false })
-			this.items = new Set(items)
 			const id = this.id
-			for (const item of items) { addEdge(item.id, id) }
+			this.items = new Uniset({ id, addEdge, delEdge, runId }, items)
 			if (config?.defer === false) { super.run() }
-		}
-
-		addItems(...items: Array<Accessor<T>>): void {
-			const { items: current_items, id } = this
-			let mutated = false
-			items.forEach((item) => {
-				if (!current_items.has(item)) {
-					current_items.add(item)
-					addEdge(item.id, id)
-					mutated = true
-				}
-			})
-			if (mutated) { runId(id) }
-		}
-
-		delItems(...items: Array<Accessor<T>>): void {
-			const { items: current_items, id } = this
-			let mutated = false
-			items.forEach((item) => {
-				if (current_items.delete(item)) {
-					delEdge(item.id, id)
-					mutated = true
-				}
-			})
-			if (mutated) { runId(id) }
 		}
 
 		static create<T>(
@@ -69,16 +121,16 @@ export const UnisetStateSignal_Factory = (ctx: Context) => {
 		): [
 				idUniset: number,
 				getUniset: Accessor<Set<T>>,
-				addItems: Identifiable<(...items: Array<Accessor<T>>) => void>,
-				delItems: Identifiable<(...items: Array<Accessor<T>>) => void>,
+				items: UnisetSignal<T>["items"],
 			] {
 			const new_signal = new this<T>(items, config)
 			return [
 				new_signal.id,
 				new_signal.bindMethod("get"),
-				new_signal.bindMethod("addItems"),
-				new_signal.bindMethod("delItems"),
+				new_signal.items
 			]
 		}
 	}
 }
+
+// DONE: consider whether or not to the "State" part in the names should be dropped.
