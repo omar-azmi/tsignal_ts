ea59bd6827d5c78461b3e9709061692b2e8455c8
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-06-16 15:59:16 +0300

    - implement a `List` data structure, that behaves like python-style lists (capable of in-between deletion and insertion). - implement an `RcList` that reference counts the multiplicity (amount of duplicates) of each unique element inside of the list array.   this class is needed by a list-style collection signal, so that it can know when the dependency on a certain child-accessor has ended, so that it can delete the gaph edge.   on the other hand, it will also allow us to seamlessly register new dependencies (when their reference counting (`rc`) comes up as 0).

--- src/deps.ts
+++ src/deps.ts
@@ -26,3 +26,241 @@ export type Stringifyable = { toString(): string }
 
 // TODO: add multiple logging options: such as one for `Signal.get` logging, and one for `Context.updateFireCycle`, etc...
 // TODO: add a link to license in `readme.md`
+
+// TODO: import the following from the newer version of `kitchensink`
+import { bind_map_delete, bind_map_get, bind_map_set } from "jsr:@oazmi/kitchensink@0.7.5/binder"
+import { array_from } from "jsr:@oazmi/kitchensink@0.7.5/builtin_aliases_deps"
+import { modulo } from "jsr:@oazmi/kitchensink@0.7.5/numericmethods"
+
+
+/** a very simple python-like `List`s class, that allows for in-between insertions, deletions, and replacements, to keep the list compact. */
+export class List<T> extends Array<T> {
+	/** inserts an item at the specified index, shifting all items ahead of it one position to the front. <br>
+	 * negative indices are also supported for indicating the position of the newly added item _after_ the array's length has incremented.
+	 * 
+	 * @example
+	 * ```ts
+	 * const arr = new List(0, 1, 2, 3, 4)
+	 * arr.insert(-1, 5) // === [0, 1, 2, 3, 4, 5] // similar to pushing
+	 * arr.insert(-2, 4.5) // === [0, 1, 2, 3, 4, 4.5, 5]
+	 * arr.insert(1, 0.5) // === [0, 0.5, 1, 2, 3, 4, 4.5, 5]
+	 * ```
+	*/
+	insert(index: number, item: T): void {
+		const i = modulo(index, this.length)
+		this.splice(i, 0, item)
+	}
+
+	/** deletes an item at the specified index, shifting all items ahead of it one position to the back. <br>
+	 * negative indices are also supported for indicating the deletion index from the end of the array.
+	 * 
+	 * @example
+	 * ```ts
+	 * const arr = new List(0, 0.5, 1, 2, 3, 4, 4.5, 5)
+	 * arr.delete(-1) // === [0, 0.5, 1, 2, 3, 4, 4.5] // similar to popping
+	 * arr.delete(-2) // === [0, 0.5, 1, 2, 3, 4.5]
+	 * arr.delete(1) // === [0, 1, 2, 3, 4.5]
+	 * ```
+	*/
+	delete(index: number): T | undefined {
+		return this.splice(index, 1)[0]
+	}
+
+	/** get an item at the specified `index`. <br>
+	 * this is equivalent to using index-based getter: `my_list[index]`.
+	*/
+	get(index: number): T | undefined { return this[index] }
+
+	/** sets the value at the specified index. <br>
+	 * prefer using this method instead of index-based assignment, because subclasses may additionally cary out more operations with this method.
+	 * and for attaining compatibility between `List` and its subclasses, it would be in your best interest to use the `set` method.
+	 * - **not recommended**: `my_list[index] = "hello"`
+	 * - **preferred**: `my_list.set(index, "hello")`
+	*/
+	set(index: number, value: T): T { return (this[index] = value) }
+
+	static from<T, U = T>(arrayLike: ArrayLike<T>, mapfn?: (v: T, k: number) => U, thisArg?: any): List<U> {
+		const new_list = new this<U>()
+		new_list.push(...array_from(arrayLike, mapfn!, thisArg))
+		return new_list
+	}
+
+	static of<T>(...items: T[]): List<T> {
+		return this.from<T>(items)
+	}
+}
+
+/** a specialized list that keeps track of the number of duplicates of each item in the list, similar to a reference counter.
+ * 
+ * this class automatically updates the reference counter on any mutations to the list at `O(log(n))`, where `n` is the number of unique items. <br>
+ * note that you __must__ use the {@link set} method for index-based assignment, otherwise the class will not be able track the changes made.
+ * - **don't do**: `my_list[index] = "hello"`
+ * - **do**: `my_list.set(index, "hello")`
+ * 
+ * @example
+ * ```ts
+ * class TrackedList<T> extends RcList<T> {
+ * 	public onAdded(item: T): void {
+ * 		console.log(`new item introduced: ${item}`)
+ * 	}
+ * 
+ * 	public onDeleted(item: T): void {
+ * 		console.log(`item completely removed: ${item}`)
+ * 	}
+ * }
+ * 
+ * const list = new TrackedList<number>()
+ * list.push(1, 2, 2, 3)
+ * // logs: "new item introduced: 1", "new item introduced: 2", "new item introduced: 3"
+ * 
+ * list.pop() // removes 3
+ * // logs: "item completely removed: 3"
+ * 
+ * list.splice(0, 1) // removes 1
+ * // logs: "item completely removed: 1"
+ * 
+ * list.unshift(4, 4, 5)
+ * // logs: "new item introduced: 4", "new item introduced: 5"
+ * 
+ * list.shift() // removes 4
+ * // logs: "item completely removed: 4"
+ * 
+ * list.set(0, 6) // replaces 2 with 6
+ * // logs: "item completely removed: 2", "new item introduced: 6"
+ * 
+ * list.set(99, 9999) // `list[99] = 9999`, in addition to extending the length of the list to `100`
+ * // logs: "new item introduced: 99"
+ * // the reference counter of `undefined` is now `95`, because the length of the list was extended by `96` elements,
+ * // and the final element (index `99`) was assigned the value of `9999`.
+ * // note that `onAdded` is not called for `undefined` elements that are introduced as a consequence of the list extending after assignment.
+ * // but `onAdded` will be called when the user _actually_ inserts an `undefined` element via direct mutation methods.
+ * ```
+*/
+export class RcList<T> extends List<T> {
+	/** the reference counting `Map`, that bookkeeps the multiplicity of each item in the list. */
+	protected readonly rc: Map<T, number> = new Map()
+
+	/** get the reference count (multiplicity) of a specific item in the list. */
+	readonly getRc = bind_map_get(this.rc)
+
+	/** set the reference count of a specific item in the list. */
+	protected readonly setRc = bind_map_set(this.rc)
+
+	/** delete the reference counting of a specific item in the list. a `true` is returned if the item did exist in {@link rc}, prior to deletion. */
+	protected readonly delRc = bind_map_delete(this.rc)
+
+	constructor(...args: ConstructorParameters<typeof List<T>>) {
+		super(...args)
+		this.incRcs(...this)
+	}
+
+	/** this overridable method gets called when a new unique item is determined to be added to the list. <br>
+	 * this method is called _before_ the item is actually added to the array, but it is executed right _after_ its reference counter has incremented to `1`. <br>
+	 * avoid accessing or mutating the array itself in this method's body (consider it an undefined behavior).
+	 * 
+	 * @param item the item that is being added.
+	*/
+	protected onAdded(item: T): void { }
+
+	/** this overridable method gets called when a unique item (reference count of 1) is determined to be removed from the list. <br>
+	 * this method is called _before_ the item is actually removed from the array, but it is executed right _after_ its reference counter has been deleted. <br>
+	 * avoid accessing or mutating the array itself in this method's body (consider it an undefined behavior).
+	 * 
+	 * @param item the item that is being removed.
+	*/
+	protected onDeleted(item: T): void { }
+
+	/** increments the reference count of each item in the provided array of items.
+	 * 
+	 * @param items the items whose counts are to be incremented.
+	*/
+	protected incRcs(...items: T[]) {
+		const { getRc, setRc } = this
+		items.forEach((item) => {
+			const new_count = (getRc(item) ?? 0) + 1
+			setRc(item, new_count)
+			if (new_count === 1) { this.onAdded(item) }
+		})
+	}
+
+	/** decrements the reference count of each item in the provided array of items.
+	 * 
+	 * @param items the items whose counts are to be decremented.
+	*/
+	protected decRcs(...items: T[]) {
+		const { getRc, setRc, delRc } = this
+		items.forEach((item) => {
+			const new_count = (getRc(item) ?? 0) - 1
+			if (new_count > 0) {
+				setRc(item, new_count)
+			} else {
+				delRc(item)
+				this.onDeleted(item)
+			}
+		})
+	}
+
+	push(...items: T[]): number {
+		this.incRcs(...items)
+		return super.push(...items)
+	}
+
+	pop(): T | undefined {
+		const
+			previous_length = this.length,
+			item = super.pop()
+		if (this.length < previous_length) { this.decRcs(item as T) }
+		return item
+	}
+
+	shift(): T | undefined {
+		const
+			previous_length = this.length,
+			item = super.shift()
+		if (this.length < previous_length) { this.decRcs(item as T) }
+		return item
+	}
+
+	unshift(...items: T[]): number {
+		this.incRcs(...items)
+		return super.unshift(...items)
+	}
+
+	splice(start: number, deleteCount?: number, ...items: T[]): T[] {
+		const removed_items = super.splice(start, deleteCount as number, ...items)
+		this.incRcs(...items)
+		this.decRcs(...removed_items)
+		return removed_items
+	}
+
+	/** sets the value at the specified index, updating the counter accordingly. <br>
+	 * always use this method instead of index-based assignment, because the latter is not interceptable (except when using proxies):
+	 * - **don't do**: `my_list[index] = "hello"`
+	 * - **do**: `my_list.set(index, "hello")`
+	*/
+	set(index: number, value: T): T {
+		const
+			old_value = super.get(index),
+			old_length = this.length,
+			increase_in_array_length = (index + 1) - old_length
+		if (increase_in_array_length === 1) {
+			// we handle this special case separately, because it would be more performant this way,
+			// and the `onDelete` method will not get called (due to `this.decRcs(old_value)`) for the just recently added `undefined` element (which is also immediately deleted)
+			this.push(value)
+		} else if ((value !== old_value) || (increase_in_array_length > 1)) {
+			value = super.set(index, value)
+			this.incRcs(value)
+			if (increase_in_array_length > 0) {
+				// if the array's length has extended due to the assignment,
+				// then we shall increment the count of `undefined` items, by the amount the array was extended by.
+				const { getRc, setRc } = this
+				setRc(undefined as T, (getRc(undefined as T) ?? 0) + increase_in_array_length)
+			}
+			this.decRcs(old_value as T)
+		}
+		return value
+	}
+
+	declare static from: <T, U = T>(arrayLike: ArrayLike<T>, mapfn?: (v: T, k: number) => U, thisArg?: any) => RcList<U>
+	declare static of: <T>(...items: T[]) => RcList<T>
+}
